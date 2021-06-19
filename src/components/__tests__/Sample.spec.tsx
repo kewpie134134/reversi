a6d08c5abac0e5dfdb861d1019ab00016d60f9ff
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-06-19 21:57:37 +0900

    [update] jest のサンプル作成

--- src/components/__tests__/Sample.spec.tsx
+++ src/components/__tests__/Sample.spec.tsx
@@ -4,9 +4,15 @@ import Sample, { square } from '../Sample';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('Sample.tsx テスト', () => {
+  let sampleComponent: ShallowWrapper;
   beforeEach(() => {
-    const sampleComponent: ShallowWrapper = shallow(<Sample />);
+    sampleComponent = shallow(<Sample />);
   });
+
+  it('<Sample /> div タグテスト', () => {
+    expect(sampleComponent.find('div').text()).toEqual('Sample');
+  });
+
   it('square 関数テスト', () => {
     const result = square(3);
     const expected = 9;

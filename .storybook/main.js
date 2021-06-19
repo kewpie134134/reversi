87f09a2e34d74b378c3aad05a61fdce1571f4914
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-06-19 22:55:42 +0900

    [add] storybook を導入

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.@(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+  ],
+  typescript: {
+    reactDocgen: 'none',
+  },
+};
---
+++ src/components/Sample.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Sample from './Sample';
+
+const title = {
+  title: 'Sample',
+};
+
+export const showAppTitle = () => <Sample />;
+
+export default title;

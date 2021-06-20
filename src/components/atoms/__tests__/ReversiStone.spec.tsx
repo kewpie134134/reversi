d92e7ce914976cf03b85a04be68cc1f51e3fe578
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-06-20 15:33:20 +0900

    [update] ReversiStone を実装

--- src/components/atoms/__tests__/ReversiStone.spec.tsx
+++ src/components/atoms/__tests__/ReversiStone.spec.tsx
@@ -3,8 +3,22 @@ import ReversiStone from '../ReversiStone';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('ReversiStone.tsx テスト', () => {
-  let sampleComponent: ShallowWrapper;
-  beforeEach(() => {
-    sampleComponent = shallow(<ReversiStone />);
+  it('props.color が black のとき、黒石のイメージを表示する', () => {
+    const wrapper: ShallowWrapper = shallow(<ReversiStone color={'black'} />);
+    expect(wrapper.find('img').props().src).toEqual(
+      '/assets/images/blackStone.svg'
+    );
+  });
+
+  it('props.color が white のとき、白石のイメージを表示する', () => {
+    const wrapper: ShallowWrapper = shallow(<ReversiStone color={'white'} />);
+    expect(wrapper.find('img').props().src).toEqual(
+      '/assets/images/whiteStone.svg'
+    );
+  });
+
+  it('props.color が green のとき、img タグが表示されない', () => {
+    const wrapper: ShallowWrapper = shallow(<ReversiStone color={'green'} />);
+    expect(wrapper.find('img').exists()).toBeFalsy();
   });
 });

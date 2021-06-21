5437e649abe37f6f1164efefbef2e417354e9054
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-22 07:15:32 +0900

    [update] jest の CSS モジュールを TypeScript に変更

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
   },
 };

7e744e331a1c10bf9acd9e69bbe7d1fe354c5714
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-23 07:16:19 +0900

    [add] StoneOnTile のテスト作成

--- src/components/molecules/__tests__/StoneOnTile.spec.tsx
+++ src/components/molecules/__tests__/StoneOnTile.spec.tsx
@@ -3,11 +3,31 @@ import StoneOnTile from '../StoneOnTile';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('StoneOnTile.tsx テスト', () => {
-  it('黒石が表示される', () => {
+  it('ReversiStone の color props が black であること', () => {
     const wrapper: ShallowWrapper = shallow(
       <StoneOnTile name={'blackStone'} />
     );
-    console.log(wrapper.html());
-    expect(wrapper.find('div').text()).toEqual('<ReversiStone />');
+    expect(wrapper.find('ReversiStone').props().color).toEqual('black');
+  });
+
+  it('ReversiStone の color props が white であること', () => {
+    const wrapper: ShallowWrapper = shallow(
+      <StoneOnTile name={'whiteStone'} />
+    );
+    expect(wrapper.find('ReversiStone').props().color).toEqual('white');
+  });
+
+  it('ExpectedStone の color props が black であること', () => {
+    const wrapper: ShallowWrapper = shallow(
+      <StoneOnTile name={'blackMarker'} />
+    );
+    expect(wrapper.find('ExpectedStone').props().color).toEqual('black');
+  });
+
+  it('ExpectedStone の color props が white であること', () => {
+    const wrapper: ShallowWrapper = shallow(
+      <StoneOnTile name={'whiteMarker'} />
+    );
+    expect(wrapper.find('ExpectedStone').props().color).toEqual('white');
   });
 });

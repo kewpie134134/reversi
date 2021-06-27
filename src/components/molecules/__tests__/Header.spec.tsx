951fa7a3c1c4e3efd0365430269582e4870a2302
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-27 16:25:44 +0900

    [update] Header のテスト作成

--- src/components/molecules/__tests__/Header.spec.tsx
+++ src/components/molecules/__tests__/Header.spec.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Header from '../Header';
+import Head from 'next/head';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('Header.tsx テスト', () => {
@@ -8,10 +12,13 @@ describe('Header.tsx テスト', () => {
     wrapper = shallow(<Header />);
   });
 
+  it('Head の text が Reversi Game であること', () => {
+    expect(wrapper.find(Head).find('title').text()).toEqual('Reversi Game');
+  });
+
   it('div の text が Reversi Game であること', () => {
-    console.log(wrapper.html());
     expect(
-      wrapper.find('div').find('header').find('div').find('h6').text
-    ).toEqual('E');
+      wrapper.find('div').find(AppBar).find(Toolbar).find(Typography).text()
+    ).toEqual('Reversi Game');
   });
 });

8f04d9341a5bdabf00d34fa4b96d44d384e7c6f1
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-27 18:38:59 +0900

    [update] next/image 使用時に Storybook でも画像を表示できるように修正

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import * as nextImage from 'next/image';
 const addParameters = require('@storybook/react').addParameters;
 
 addParameters({
@@ -7,4 +8,9 @@ addParameters({
         ? 0
         : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
+});
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: (props) => <img {...props} />,
 });

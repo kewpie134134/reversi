1b84688ded6432d1cfe701f7a5d043e8b7efadfe
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-27 18:16:30 +0900

    [update] テスト修正

--- src/components/atoms/__tests__/Piece.spec.tsx
+++ src/components/atoms/__tests__/Piece.spec.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Piece from '../Piece';
+import Image from 'next/image';
 import { shallow, ShallowWrapper } from 'enzyme';
 
 describe('Piece.tsx テスト', () => {
   it('props.color が black のとき、黒石のイメージを表示する', () => {
     const wrapper: ShallowWrapper = shallow(<Piece color={'black'} />);
-    expect(wrapper.find('img').props().src).toEqual(
+    expect(wrapper.find(Image).props().src).toEqual(
       '/assets/images/blackPiece.svg'
     );
   });
 
   it('props.color が white のとき、白石のイメージを表示する', () => {
     const wrapper: ShallowWrapper = shallow(<Piece color={'white'} />);
-    expect(wrapper.find('img').props().src).toEqual(
+    expect(wrapper.find(Image).props().src).toEqual(
       '/assets/images/whitePiece.svg'
     );
   });
 
   it('props.color が white, black 以外のとき、img タグが表示されない', () => {
     const wrapper: ShallowWrapper = shallow(<Piece color={'green'} />);
-    expect(wrapper.find('img').exists()).toBeFalsy();
+    expect(wrapper.find(Image).exists()).toBeFalsy();
   });
 });

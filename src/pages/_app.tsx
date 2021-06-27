5b849f527aa627ecd1b2292581a8a40967b73839
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-06-27 13:53:39 +0900

    [update] 更新しても warning 蛾でないように修正した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 };
 export default MyApp;

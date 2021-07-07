fb9ae7b91359d4cada326b76a0e093bf7ece945b
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-07-07 16:02:20 +0900

    [update] 連想配列で number を入れる箱を作成

--- src/components/organisms/ReversiBoard.tsx
+++ src/components/organisms/ReversiBoard.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const ReversiBoard = (): JSX.Element => <>ReversiBoard</>;
+const ReversiBoard = (): JSX.Element => {
+  // 盤面作成
+  const array: number[][] = new Array<number[]>(8);
+  for (let x = 0; x < array.length; x += 1) {
+    array[x] = new Array<number>(8);
+  }
+
+  // 盤面初期化
+  for (let x = 0; x < array.length; x += 1) {
+    for (let y = 0; y < array.length; y += 1) {
+      array[x][y] = 0;
+    }
+  }
+
+  console.log(array);
+
+  return <>{array}</>;
+};
 
 export default ReversiBoard;

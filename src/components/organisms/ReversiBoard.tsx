129f26ebc067b654d242e9d1edc55d8bd47aeb36
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-07-07 17:19:33 +0900

    [update] オセロのコマを縦に並べた

--- src/components/organisms/ReversiBoard.tsx
+++ src/components/organisms/ReversiBoard.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import Tile from 'components/atoms/Tile';
+import PieceOnTile from 'components/molecules/PieceOnTile';
 
 const ReversiBoard = (): JSX.Element => {
   // 盤面作成
-  const array: number[][] = new Array<number[]>(8);
-  for (let x = 0; x < array.length; x += 1) {
-    array[x] = new Array<number>(8);
+  const reversiBoard: JSX.Element[][] = new Array<JSX.Element[]>(8);
+  for (let x = 0; x < reversiBoard.length; x += 1) {
+    reversiBoard[x] = new Array<JSX.Element>(8);
   }
 
-  // 盤面初期化
-  for (let x = 0; x < array.length; x += 1) {
-    for (let y = 0; y < array.length; y += 1) {
-      array[x][y] = 0;
+  // 盤面初期化(key 値は x*10+y とする)
+  for (let x = 0; x < reversiBoard.length; x += 1) {
+    for (let y = 0; y < reversiBoard.length; y += 1) {
+      reversiBoard[x][y] = <Tile key={x * 10 + y} />;
     }
   }
 
-  console.log(array);
+  // ゲームスタート時の状態に盤面を準備
+  reversiBoard[3][3] = <PieceOnTile color="black" key={33} />;
+  reversiBoard[3][4] = <PieceOnTile color="white" key={34} />;
+  reversiBoard[4][3] = <PieceOnTile color="black" key={43} />;
+  reversiBoard[4][4] = <PieceOnTile color="white" key={44} />;
 
-  return <>{array}</>;
+  return <>{reversiBoard}</>;
 };
 
 export default ReversiBoard;

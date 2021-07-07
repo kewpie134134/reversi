916414022e1d09c84fbdf8130d0b7d73b1280273
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-07-07 21:21:28 +0900

    [add] ReversiBoard の Storybook を作成

---
+++ src/components/organisms/stories/ReversiBoard.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReversiBoard from '../ReversiBoard';
+
+const title = {
+  title: 'ReversiBoard',
+};
+
+export const reversiBoard = (): JSX.Element => <ReversiBoard />;
+
+export default title;

cfe98d949d25e5832e0315cd19a569b2071a5e2e
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2023-02-14 23:49:59 +0900

    [fix] 不要な記述を削除

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  build: {
-    babel: {
-      plugins: [
-        [
-          '@babel/plugin-proposal-private-methods',
-          {
-            loose: true,
-          },
-          '@babel/plugin-proposal-private-property-in-object',
-          { loose: true },
-        ],
-      ],
-    },
-  },
 };
